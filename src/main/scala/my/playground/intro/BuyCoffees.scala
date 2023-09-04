09c23884afc3bf75a8ec518fe20beb84b286cd6f
Daniel <daniel5hbs@gmail.com>
2023-09-05 05:35:44 +1000

    intro

---
+++ src/main/scala/my/playground/intro/BuyCoffees.scala
@@ -0,0 +1,36 @@
+package my.playground
+package my.playground.intro
+
+object BuyCoffees:
+  @main
+  def buyCoffee(): Unit =
+    val cafe = Cafe()
+    cafe.buyCoffee(CreditCard())
+
+class Cafe:
+  def buyCoffee(cc: CreditCard): (Coffee, Charge) =
+    val cup = Coffee()
+    (cup, Charge(cc, cup.price))
+
+  def buyCoffees(cc: CreditCard, n: Int): (List[Coffee], Charge) =
+    val purchases: List[(Coffee, Charge)] = List.fill(n)(buyCoffee(cc))
+    val (coffees, charges) = purchases.unzip
+    val charge = charges.reduce((c1, c2) => c1.combine(c2))
+    (coffees, charge)
+
+  def coalesce(charges: List[Charge]): List[Charge] =
+    charges.groupBy(_.cc).values.map(_.reduce(_.combine(_))).toList
+
+class CreditCard:
+  def charge(amount: Double): Unit =
+    println("Charging: " + amount)
+
+case class Charge(cc: CreditCard, amount: Double):
+  def combine(other: Charge): Charge =
+    if other.cc == cc then
+      Charge(cc, amount + other.amount)
+    else
+      throw new Exception("cannot combine charges with different credit cards")
+class Coffee:
+  val price: Double = 2.0
+
